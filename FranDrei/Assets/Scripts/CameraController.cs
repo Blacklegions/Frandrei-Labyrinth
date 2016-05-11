b2cbd7ca52ba71df2e76071ea6febde9059a57ef
Fred <talumbee@gmail.com>
2016-05-11 18:21:17 -0400

    baton vert
    player et camera scripts update

---
+++ FranDrei/Assets/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public float lookSmooth = 0.09f;
+    public Vector3 offsetFromTarget = new Vector3(0, 0, 0);
+    public float xTilt = 10;
+
+    Vector3 destination = Vector3.zero;
+    PlayerCtrl charController;
+    float rotateVel = 0;
+
+    void Start()
+    {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t)
+    {
+        target = t;
+
+        if (target != null)
+        {
+            if (target.GetComponent<PlayerCtrl>())
+            {
+                charController = target.GetComponent<PlayerCtrl>();
+            }
+            else
+            {
+                Debug.LogError("need char controller");
+            }
+        }
+        else
+            Debug.LogError("camera no target");
+    }
+
+    void LateUpdate()
+    {
+        //moving n rotation
+        MoveToTarget();
+        LookAtTarget();
+    }
+
+    void MoveToTarget()
+    {
+        destination = charController.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+}
+
